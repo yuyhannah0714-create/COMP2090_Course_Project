44aabfe8c9e286169c259cf3a3a76a95610c57b0
yuyhannah0714-create <yuyhannah0714@gmail.com>
2026-03-08 14:47:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from account import Account, SavingsAccount
 from bank_system import BankSystem
 
-def main():
-    bank = BankSystem()
-   
-    acc1 = Account("A101", 1000)
-    acc2 = SavingsAccount("S202", 5000)
-    
-    bank.add_account(acc1)
-    bank.add_account(acc2)
+bank = BankSystem()
 
-    print("---bank system test---")
-    bank.get_acc_balance("A101")
-    bank.acc_deposit("A101", 200)
-    bank.transfer("S202", "A101", 1500)
-    bank.get_acc_balance("A101")
-    bank.get_acc_balance("S202")
+a = Account("1", 1000)
+b = SavingsAccount("2", 5000)
 
-if __name__ == "__main__":
-    main()
+bank.add_account(a)
+bank.add_account(b)
+
+print("test")
+
+bank.get_acc_balance("1")
+
+bank.acc_deposit("1",200)
+
+bank.transfer("2","1",1500)
+
+print("after transfer")
+
+bank.get_acc_balance("1")
+bank.get_acc_balance("2")
+
+print("done")
